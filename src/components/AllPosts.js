3e30bfa463e063a394621388e97febf2f4145de0
Dan Zera <dczera@gmail.com>
2017-12-10 17:48:47 -0600

    return posts from reducer once API request has been fulfilled, utilize lodash to render list of posts

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import _ from 'lodash';
 
 class AllPosts extends Component {
 	componentDidMount() {
-		console.log(this.props);
 		this.props.fetchPosts();
+	}
+
+	renderPosts() {
+		return _.map(this.props.posts, post => {
+			console.log('mapping post', post);
+			return <li className="list-group-item" key={post.id}>{post.title}</li>
+		});
 	}
 
 	render() {
 		console.log('AllPosts props', this.props);
+		if (!this.props.posts) {
+			return <div>Loading Posts...</div>
+		}
 		return (
 			<div>
-				AllPosts
+				<h3>Current Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 			</div>
 		);
 	}
 }
 
 function mapStateToProps(state) {
+	console.log('current posts', state.posts);
 	return { posts: state.posts }
 }
 
-export default connect(null, { fetchPosts })(AllPosts);
+export default connect(mapStateToProps, { fetchPosts })(AllPosts);
